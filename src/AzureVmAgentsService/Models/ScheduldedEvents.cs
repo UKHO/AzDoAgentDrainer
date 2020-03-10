441f4d418183e9bff5ec0b478a5f2d54a91f0e9b
Marc Denman <marc.denman@ukho.gov.uk>
2020-03-10 22:01:17 +0000

    NotBefore should be a string

--- src/AzureVmAgentsService/Models/ScheduldedEvents.cs
+++ src/AzureVmAgentsService/Models/ScheduldedEvents.cs
@@ -6,10 +6,10 @@ namespace AzureVmAgentsService.Models
     public class ScheduldedEvents
     {
         public string EventId { get; set; }
+        public string EventStatus { get; set; }
         public string EventType { get; set; }
         public string ResourceType { get; set; }
-        public List<string> Resources { get; set; }
-        public string EventStatus { get; set; }
-        public DateTime NotBefore { get; set; }
+        public List<string> Resources { get; set; }       
+        public string NotBefore { get; set; }
     }
 }
